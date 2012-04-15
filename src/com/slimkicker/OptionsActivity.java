abc711c30a81ecb6e8c9b049d22b080b4a372cb8
Henley Chiu <henley.chiu@gmail.com>
2012-04-15 12:48:48 -0400

    add stuff

---
+++ src/com/slimkicker/OptionsActivity.java
@@ -0,0 +1,16 @@
+package com.slimkicker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author www.androidcookers.co.cc
+ * {@link www.androidcookers.co.cc}
+ *
+ */
+public class OptionsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab2);               
+    }
+}
